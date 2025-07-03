bdcde83150920c4d00bb498b2c597f5f17953874
Sz19-pixel <trytry11101@gmail.com>
2025-07-03 20:59:50 +0300

    Create build.gradle.kts

---
+++ plugins/StremioC/build.gradle.kts
@@ -0,0 +1,16 @@
+// use an integer for version numbers
+version = 13
+
+cloudstream {
+    language = "en"
+    description = "[!] Requires Setup \n- StremioX allows you to use stream addons \n- StremioC allows you to use catalog addons"
+    authors = listOf("Hexated")
+
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/master/StremioX/icon.png"
+}
